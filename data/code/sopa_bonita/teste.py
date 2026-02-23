c1b1740ebffe822cf774f4038519dcbcdb10dea0
Andrei Laskievic <alaskievic@gmail.com>
2026-02-22 23:32:33 -0500

    descance em paz principe baelor

--- data/code/sopa_bonita/teste.py
+++ data/code/sopa_bonita/teste.py
@@ -2,57 +2,9 @@ from bs4 import BeautifulSoup, Tag, NavigableString
 from urllib.request import urlopen
 import pandas as pd
 import requests
-
-url = "http://www.estacoesferroviarias.com.br/m/mooca.htm"
-page = urlopen(url)
-html = page.read()
-soup = BeautifulSoup(html, "html.parser")
-main_text = soup.get_text()
-
-# date of establishment
-date_tag = soup.find('b', string='Inauguração:')
-date_node = date_tag.next_sibling
-if date_node and isinstance(date_node, NavigableString):
-    date_data = date_node.strip()
-    print(f"{date_data}")
-
-# municipality name
-mun_tag = soup.find(class_='municipio')
-mun_data = mun_tag.get_text(strip=True).replace("Município de ", "")
-print(f"{mun_data}")
-
-# statation name
-station_tag = soup.find("span", class_="estacao")
-data_station = station_tag.get_text(strip=True)
-print(data_station)
-
-# railway names
-rail_tag = soup.find(class_="estacao")
-data_rail = rail_tag.get_text(strip=True)
-data_rail = data_rail.replace("\n", "").replace("\r", "").replace(" ", "")
-print(data_rail)
-
-# branch name
-branch_tag = soup.find_all('strong')[0]
-data_branch = branch_tag.get_text(strip=True)
-data_branch = data_branch.replace(" -", "")
-print(data_branch)
-
-test_df = pd.DataFrame({'municipality': mun_data, "start_date": date_data, "station": data_station, "railway": data_rail, "branch": data_branch}, index=[0])
-test_df.to_csv("test.csv", index=False)
-
-
-
-from bs4 import BeautifulSoup, Tag, NavigableString
-from urllib.request import urlopen
-import pandas as pd
-import requests
-url = "http://www.estacoesferroviarias.com.br/a/est-a.htm"
-r = requests.get(url)
-html_content = r.text
+import re
 
 # Sao Paulo
-
 # Loop over all pages based on alphabet and extract the links to the station pages
 alpha_list = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u"]
 
@@ -76,53 +28,55 @@ for url in url_list:
             htm_links = link.find('a').get('href').replace("../", "")
         htm_list.append(htm_links)
 
+mun_data_list = []
+station_data_list = []
+rail_data_list = []
+branch_data_list = []
+date_data_list = []
 for htm in htm_list:
-    url = f"http://www.estacoesferroviarias.com.br/{htm}"
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    # branch name
-    branch_tag = soup.find_all('strong')[0]
-    branch_data = branch_tag.get_text(strip=True)
-    branch_data = branch_data.replace(" -", "")
-    print(branch_data)
-    # date of establishment
-    date_tag = soup.find('b', string='Inauguração:')
-    date_node = date_tag.next_sibling
-    if date_node and isinstance(date_node, NavigableString):
-        date_data = date_node.strip()
-        print(f"{date_data}")
-    # municipality name
-    mun_tag = soup.find(class_='municipio')
-    mun_data = mun_tag.get_text(strip=True).replace("Município de ", "")
-    print(f"{mun_data}")
-    # statation name
-    station_tag = soup.find("span", class_="estacao")
-    stationn_data = station_tag.get_text(strip=True)
-    print(stationn_data)
-    # railway names
-    rail_tag = soup.find(class_="estacao")
-    rail_data = rail_tag.get_text(strip=True)
-    rail_data = rail_data.replace("\n", "").replace("\r", "").replace(" ", "")
-    print(rail_data)
+    try:
+        if htm[1] != "/":
+            fix_htm = f"{htm[0]}/{htm}"
+        elif htm[1] == "w":
+            fix_htm = f"v/{htm}"
+        else:
+            fix_htm = htm
+        url = f"http://www.estacoesferroviarias.com.br/{fix_htm}"
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        url = f"http://www.estacoesferroviarias.com.br/{fix_htm}"
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        # muncipality and station name
+        mun_station_tag = soup.find_all("tr", class_="estacao")[1]
+        mun_station_data = mun_station_tag.get_text(strip=True).replace("\n", "").replace("\r", "").replace(" ", "")
+        mun_station_split = mun_station_data.partition("Municípiode")
+        station_data = mun_station_split[0]
+        mun_data = mun_station_split[2]
+        mun_data_list.append(mun_data)
+        station_data_list.append(station_data)
+        print(f"{station_data}")
+        print(f"{mun_data}")
+        # railway names
+        rail_tag = soup.find_all("tr", class_="estacao")[0]
+        rail_data = rail_tag.get_text(strip=True).replace("\n", "").replace("\r", "").replace(" ", "")
+        rail_data_list.append(rail_data)
+        print(f"{rail_data}")
+        # branch name
+        branch_tag = soup.find_all("td", class_="dados")[1]
+        branch_data = branch_tag.get_text(strip=True).replace(" -", "").replace("\n", "").replace("\r", "")
+        branch_data_list.append(branch_data)
+        print(f"{branch_data}")
+        # date of establishment
+        date_tag = soup.find('b', string='Inauguração:')
+        date_node = date_tag.next_sibling
+        if date_node and isinstance(date_node, NavigableString):
+            date_data = date_node.strip()
+            print(f"{date_data}")
+        date_data_list.append(date_data)
+    except:
+        print(url)
+    continue
 
-print(branch_data)
-print(htm_list)
-sp_df = pd.DataFrame({'municipality': mun_data, "start_date": date_data, "station": stationn_data, "railway": rail_data, "branch": branch_data}, index=[0])
+sp_df = pd.DataFrame({'municipality': mun_data_list, "start_date": date_data_list, "station": station_data_list, "railway": rail_data_list, "branch": branch_data_list})
 sp_df.to_csv("sp_station.csv", index=False)
-
-
-
-
-for htm in htm_list:
-    url = f"http://www.estacoesferroviarias.com.br/{htm}"
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    # branch name
-    branch_tag = soup.find_all('strong')[0]
-    branch_data = branch_tag.get_text(strip=True)
-    branch_data = branch_data.replace(" -", "")
-    print(branch_data)
-
-
-
-print(soup.prettify())
